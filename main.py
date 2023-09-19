ff1a2c6bbe7c5640694c71c200942d4be78b640a
Tomas Marmay <tomasmarmay21@gmail.com>
2023-09-19 10:37:29 -0300

    estructurado proyecto

--- main.py
+++ main.py
@@ -6,31 +6,16 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 
+from api.home import router as home_router 
+
 app = FastAPI()
 
 #configuro directorio de archivos estaticos 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 #configuro directorio de paginas dinamicas
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="/templates")
 
-@app.get("/", response_class=HTMLResponse)
-async def home(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
-
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
-
-
-#hace pattern maching entra en el primero
-@app.get("/users")
-async def read_users():
-    return ["Rick", "Morty"]
-@app.get("/users")
-async def read_users2():
-    return ["Bean", "Elfo"]
-
-
+# Agregar el router de usuarios a la aplicación
+app.include_router(home_router, prefix="/api")
 
