80ac86fde3dc6dee6388e2751a561213f6de930f
irmrz <ignaciotramirez02@gmail.com>
2023-09-28 21:31:03 -0300

    agregado css

--- frontend/src/components/Formularios/CrearPartida/FormCrearPartida.jsx
+++ frontend/src/components/Formularios/CrearPartida/FormCrearPartida.jsx
@@ -1,4 +1,6 @@
 import {useForm} from "react-hook-form"
+import CustomButton from "../../Boton/CustomButton";
+import "./FormCrearPartida.css"
 
 function FormCrearPartida()
 {
@@ -26,95 +28,94 @@ function FormCrearPartida()
     });
 
     return(
-    <form onSubmit={FORM_SUBMIT}>
-        
-        <div>
-        <label>Nombre de la Partida</label>
-        <input
-            type = "text"
-            name = "nombrePartida"
-            {...register("nombrePartida", {
-            required: { 
-                    value: true, 
-                    message: "Nombre de la partida requerido" 
-                },
-                maxLength: {
-                    value: 20,
-                    message: "Nombre de la partida demasiado largo"
-                },
-                minLength: {
-                    value: 4,
-                    message: "Nombre de la partida demasiado corto"
-                }
-            })}
-        />
-        {errors.nombrePartida && <p>{errors.nombrePartida.message}</p>}
-        </div>
+    <> 
+        <h2>Formulario de Creación</h2>
+        <body>
+            <form onSubmit={FORM_SUBMIT}>
+                <div>
+                    <label>Nombre de la Partida</label>
+                    <input
+                        type="text"
+                        name="nombrePartida"
+                        {...register("nombrePartida", {
+                            required: {
+                                value: true,
+                                message: "Nombre de la partida requerido"
+                            },
+                            maxLength: {
+                                value: 20,
+                                message: "Nombre de la partida demasiado largo"
+                            },
+                            minLength: {
+                                value: 4,
+                                message: "Nombre de la partida demasiado corto"
+                            }
+                        })} />
+                    {errors.nombrePartida && <p>{errors.nombrePartida.message}</p>}
+                </div>
 
-        <div>
-            <label>Mínimo de Jugadores</label>
-            <input
-                type = "number"
-                name = "minPlayers"
-                {...register("minPlayers", {
-                required: { 
-                        value: "", 
-                        message: "Mínimo de jugadores requerido" 
-                    },
-                    max: {
-                        value: 12,
-                        message: "Mínimo de jugadores demasiado alto"
-                    },
-                    min: {
-                        value: 4,
-                        message: "Mínimo de jugadores demasiado bajo"
-                    }
-                })}
-            />
-        </div>
+                <div>
+                    <label>Mínimo de Jugadores</label>
+                    <input
+                        type="number"
+                        name="minPlayers"
+                        {...register("minPlayers", {
+                            required: {
+                                value: "",
+                                message: "Mínimo de jugadores requerido"
+                            },
+                            max: {
+                                value: 12,
+                                message: "Mínimo de jugadores demasiado alto"
+                            },
+                            min: {
+                                value: 4,
+                                message: "Mínimo de jugadores demasiado bajo"
+                            }
+                        })} />
+                </div>
 
-        <div>
-            <label>Máximo de Jugadores</label>
-            <input
-                type = "number"
-                name = "maxPlayers"
-                {...register("maxPlayers", {
-                required: { 
-                        value: "", 
-                        message: "Máximo de jugadores requerido" 
-                    },
-                    max: {
-                        value: 12,
-                        message: "Máximo de jugadores demasiado alto"
-                    },
-                    min: {
-                        value: 4,
-                        message: "Máximo de jugadores demasiado bajo"
-                    }
-                })}
-            />
-        </div>
+                <div>
+                    <label>Máximo de Jugadores</label>
+                    <input
+                        type="number"
+                        name="maxPlayers"
+                        {...register("maxPlayers", {
+                            required: {
+                                value: "",
+                                message: "Máximo de jugadores requerido"
+                            },
+                            max: {
+                                value: 12,
+                                message: "Máximo de jugadores demasiado alto"
+                            },
+                            min: {
+                                value: 4,
+                                message: "Máximo de jugadores demasiado bajo"
+                            }
+                        })} />
+                </div>
 
-        <div>
-            <label>Contraseña</label>
-            <input
-                type = "password"
-                name = "contraseña"
-                {...register("contraseña", {
-                    maxLength: {
-                        value: 20,
-                        message: "Contraseña demasiado larga"
-                    },
-                    minLength: {
-                        value: 4,
-                        message: "Contraseña demasiado corta"
-                    }
-                })}
-            />
-        </div>
-        
-        <button type="submit">Crear Partida</button>
-    </form>
+                <div>
+                    <label>Contraseña</label>
+                    <input
+                        type="password"
+                        name="contraseña"
+                        {...register("contraseña", {
+                            maxLength: {
+                                value: 20,
+                                message: "Contraseña demasiado larga"
+                            },
+                            minLength: {
+                                value: 4,
+                                message: "Contraseña demasiado corta"
+                            }
+                        })} />
+                </div>
+
+                <CustomButton label="Crear Partida" onClick={FORM_SUBMIT} />
+            </form>
+        </body></>
     );
 }
 
