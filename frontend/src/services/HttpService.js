fe59100312eb7904a463c321c447b30e8bbbe3e7
cuevasnacho <102696702+cuevasnacho@users.noreply.github.com>
2023-09-30 20:27:34 -0300

    Correccion HttpService.js
    se corrigio el puerto

--- frontend/src/services/HttpService.js
+++ frontend/src/services/HttpService.js
@@ -1,4 +1,4 @@
-const SERVER_URI = 'http://localhost:5173/';
+const SERVER_URI = 'http://localhost:8000/';
 
 const parseJSONResponse = (response) => {
   return new Promise((resolve) => response.json()
