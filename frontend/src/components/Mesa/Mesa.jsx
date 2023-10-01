508718e023fa806c03bd3bb8640a942e91fed6fa
Ignacio Cuevas <nachocuevas04@gmail.com>
2023-10-01 15:06:05 -0300

    Creacion de archivos de Mesa

---
+++ frontend/src/components/Mesa/Mesa.jsx
@@ -0,0 +1,14 @@
+import styles from './Mesa.module.css';
+// import { httpRequest } from '../../services/HttpService.js';
+
+const Mesa = () => {
+  return(  
+    <div className={styles.page}>
+      <div className={styles.Mesa}>
+          <h1>ASDASD</h1>
+      </div>
+    </div>
+  )
+};
+
+export default Mesa;
