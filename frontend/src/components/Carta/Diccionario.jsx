d29687eb8eb805479dd867cd31182e3e49ec19f2
irmrz <ignaciotramirez02@gmail.com>
2023-10-02 12:08:27 -0300

    actualizado path Diccionario

--- frontend/src/components/Carta/Diccionario.jsx
+++ frontend/src/components/Carta/Diccionario.jsx
@@ -1,6 +1,6 @@
-import analisis from '/home/ignacio/lacosa/frontend/src/media/designs/cartas/alejate/analisis.png'
-import lacosa from '/home/ignacio/lacosa/frontend/src/media/designs/cartas/alejate/lacosa.png'
-import cuerdas_podridas from '/home/ignacio/lacosa/frontend/src/media/designs/cartas/panico/cuerdas_podridas.png'
+import analisis from '../../media/designs/cartas/alejate/analisis.png'
+import lacosa from '../../media/designs/cartas/alejate/lacosa.png'
+import cuerdas_podridas from '../../media/designs/cartas/panico/cuerdas_podridas.png'
 
 const Diccionario = {
     "analisis": analisis,
---
+++ frontend/src/components/Jugador/Jugador.jsx
@@ -0,0 +1,11 @@
+
+function Jugador(props) {
+    const username = props.username;
+    const esHost = props.esHost;
+
+    return (
+        <div className="jugador">
+            
+        </div>
+    );
+}
