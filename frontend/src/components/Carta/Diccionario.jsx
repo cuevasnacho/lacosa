fd7b013507104b63159b4a5851b74bcfd0bccb69
Ignacio Cuevas <nachocuevas04@gmail.com>
2023-10-02 14:52:34 -0300

    Comenzando partida

---
+++ frontend/src/components/Carta/Diccionario.jsx
@@ -0,0 +1,13 @@
+const IMG_SRC = '../../media/designs/cartas/alejate/';
+
+import lanzallamas from '../../media/designs/cartas/alejate/lanzallamas.png';
+import analisis from '../../media/designs/cartas/alejate/analisis.png';
+import lacosa from '../../media/designs/cartas/alejate/lacosa.png';
+
+const Diccionario = {
+    "analisis": analisis,
+    "lacosa": lacosa,
+    "lanzallamas": lanzallamas
+};
+
+export default Diccionario;
--- frontend/src/components/Partida/Partida.jsx
+++ frontend/src/components/Partida/Partida.jsx
@@ -1,5 +1,11 @@
 import Mesa from '../Mesa/Mesa.jsx';
+import ManoJugador from '../ManoJugador/ManoJugador.jsx';
+import Diccionario from '../Carta/Diccionario.jsx';
+import styles from './Partida.module.css';
+import mazo from '../../media/designs/cartas/misteriosa.png';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { httpRequest } from '../../services/HttpService.js';
 
 const Partida = function () {
   const usuario = JSON.parse(sessionStorage.getItem('logged'));
@@ -10,11 +16,39 @@ const Partida = function () {
     navigate('/home');
   }
 
+  const levantarCarta = async () => {
+    try {
+      const response = await httpRequest({
+        method: 'POST',
+        service: 'levantar',
+        payload: usuario.user_id
+      });
+      
+      mycards = JSON.parse(sessionStorage.getItem('cards'));
+      mycards.push(response.id_carta);
+      window.sessionStorage.setItem('cards', JSON.stringify(mycards));
+
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const [cartas, setCartas] = useState([Diccionario['lacosa'], Diccionario['lanzallamas']]);
+
   return (
-    <>
-      <ManoJugador cartas={""}/>
-      <Mesa />
-    </>
+    <div className={styles.container}>
+      <div className={styles.manojugador}>
+        <ManoJugador cartas={cartas} jugarCarta/>
+      </div>
+      <div className={styles.mazos}>
+        <button type='button' onClick={levantarCarta}/>
+        <img src={mazo} width={130}/>
+      </div>
+      <div className={styles.jugadores}>
+
+      </div>
+    </div>
   )
 }
 
