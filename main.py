bad520a68b231913f042be8955cd158ed4dca5b3
MarcosDelhugo <marcos.delhugo@mi.unc.edu.ar>
2023-10-02 15:27:47 -0300

    Modifcando py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#levantar el servidor 
+#levantar el servidor
 #uvicorn main:app --reload
 
 from fastapi import FastAPI, Request
@@ -8,17 +8,20 @@ from fastapi.responses import HTMLResponse
 from db.database import Player as db_player
 from pony.orm import db_session
 
-from api.home import router as home_router 
+from api.home import router as home_router
 from api.discard_card import router as discard_router
 from api.show_matches import router as show_matches_router
 from api.models.user import router as user_router
+from api.start_match import router as start_match_router
 from api.models.lobby import router as lobby_router
-
+from api.start_match import router as start_match_router
 from fastapi.middleware.cors import CORSMiddleware
+from api.load_templates import load_templates
+
 app = FastAPI()
 
 origins = ["*"]
- 
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -27,16 +30,20 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-#configuro directorio de archivos estaticos 
+#configuro directorio de archivos estaticos
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 #configuro directorio de paginas dinamicas
 templates = Jinja2Templates(directory="/templates")
+
+#configuro estado base de la base de datos
+load_templates()
 
 #agregar el router de usuarios a la aplicación
 app.include_router(home_router)
 app.include_router(discard_router)
 app.include_router(show_matches_router)
 app.include_router(user_router)
+app.include_router(start_match_router)
 app.include_router(lobby_router)
-
+app.include_router(start_match_router)
