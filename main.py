1e85dedd27e6c7ea40eb0a7b55bbfdf3713f39a6
Marcos Strasorier <marcos.strasorier@mi.unc.edu.ar>
2023-10-02 12:25:45 -0300

    Se pueden unir al lobby, falta que un player sea host

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#levantar el servidor 
+#levantar el servidor
 #uvicorn main:app --reload
 
 from fastapi import FastAPI, Request
@@ -8,17 +8,18 @@ from fastapi.responses import HTMLResponse
 from db.database import Player as db_player
 from pony.orm import db_session
 
-from api.home import router as home_router 
+from api.home import router as home_router
 from api.discard_card import router as discard_router
 from api.show_matches import router as show_matches_router
 from api.models.user import router as user_router
 from api.models.lobby import router as lobby_router
+from api.request_join import router as request_join_router
 
 from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = ["*"]
- 
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -27,7 +28,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-#configuro directorio de archivos estaticos 
+#configuro directorio de archivos estaticos
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 #configuro directorio de paginas dinamicas
@@ -39,4 +40,4 @@ app.include_router(discard_router)
 app.include_router(show_matches_router)
 app.include_router(user_router)
 app.include_router(lobby_router)
-
+app.include_router(request_join_router)
