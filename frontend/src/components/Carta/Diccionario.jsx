52cd97d3dd4dadb732883820823e6c003c3bc602
irmrz <ignaciotramirez02@gmail.com>
2023-10-02 12:13:41 -0300

    carta agregada

--- frontend/src/components/Carta/Diccionario.jsx
+++ frontend/src/components/Carta/Diccionario.jsx
@@ -1,11 +1,13 @@
 import analisis from '../../media/designs/cartas/alejate/analisis.png'
 import lacosa from '../../media/designs/cartas/alejate/lacosa.png'
 import cuerdas_podridas from '../../media/designs/cartas/panico/cuerdas_podridas.png'
+import aterrador from '../../media/designs/cartas/panico/aterrador.png'
 
 const Diccionario = {
     "analisis": analisis,
     "lacosa": lacosa,
-    "cuerdas_podridas": cuerdas_podridas
+    "cuerdas_podridas": cuerdas_podridas,
+    "aterrador": aterrador
 };
 
 export default Diccionario;
