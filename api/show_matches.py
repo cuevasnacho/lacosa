4c4fff3df1538f47de1dcb4d0da7508d881b3c75
Tomas Marmay <tomasmarmay21@gmail.com>
2023-10-02 09:44:06 -0300

    campos agregados a listado de partidas

--- api/show_matches.py
+++ api/show_matches.py
@@ -18,6 +18,7 @@ class data_item(BaseModel):
     host_name : str
     max_players : int
     min_players : int
+    is_private : bool
 
 @db_session 
 def get_not_initialized_matches():
@@ -25,19 +26,20 @@ def get_not_initialized_matches():
 
     #query que obtiene de todas las partidas disponibles y los datos para completar data_item
     data_from_available_matches = orm.select( 
-        (lobby.lobby_id, match.match_id, lobby.lobby_name, lobby.lobby_pcount, lobby.lobby_max, lobby.lobby_min) 
+        (lobby.lobby_id, match.match_id, lobby.lobby_name, lobby.lobby_pcount, lobby.lobby_max, lobby.lobby_min, lobby.lobby_password) 
         for match in Match for lobby in Lobby 
             if lobby.lobby_match.match_id == match.match_id and match.match_status == match_status.NOT_INITIALIZED.value) 
 
     #crear lista de objetos data_item 
     for data in data_from_available_matches:
         match_id  = data[1] #query adicional para obtener el host_name
+        private_game = True if data[6] != None else False
         host_name = Player.select(lambda player : 
                             player.player_current_match_id.match_id == match_id and player.player_isHost).first()
 
         if host_name != None:
             response.append(data_item(lobby_id = data[0], match_id = data[1], lobby_name = data[2], number_of_players = data[3],
-                                    host_name = host_name.player_name, max_players = data[4], min_players = data[5]))        
+                                    host_name = host_name.player_name, max_players = data[4], min_players = data[5], is_private = private_game))        
     
     #convertir y devolver la lista de objetos data_item en formato json
     return json.loads(json.dumps([obj.dict() for obj in response]))
