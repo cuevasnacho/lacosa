54cd42fa5e1fc725fe2f85c609d161ebcd44e0ae
Tomas's Home <tomasmarmay21@gmail.com>
2023-10-05 18:39:28 -0300

    bug request join arreglado

--- api/lobby/request_join.py
+++ api/lobby/request_join.py
@@ -17,14 +17,12 @@ def player_update(player_id,lobby_id,match_id):
     player_get.player_isHost = False
     player_get.player_lobby = lobby_id
     player_get.player_current_match_id = match_id
-    commit()
 
 @db_session
 def lobby_upadte(lobby_id,match_id):
     lobby_get = Lobby[lobby_id]
     lobby_get.lobby_pcount = lobby_get.lobby_pcount + 1
     lobby_get.lobby_match = match_id
-    commit()
 
 @db_session
 def get_match_id(lobby_id):
@@ -45,13 +43,14 @@ async def unirse_lobby(lobby_id : int, player_id : int):
         message = "El lobby esta lleno"
         status_code = 406
         return JSONResponse(content=message, status_code=status_code)
+    
+    with db_session:
+        #cambiar esto del jugador
+        match_id = get_match_id(lobby_id)
+        player_update(player_id,lobby_id,match_id)
 
-    #cambiar esto del jugador
-    match_id = get_match_id(lobby_id)
-    player_update(player_id,lobby_id,match_id)
-
-    #cambiar estado lobby
-    lobby_upadte(lobby_id, match_id)
-
+        #cambiar estado lobby
+        lobby_upadte(lobby_id, match_id)
+        commit()
     return JSONResponse(content=f"jugador {player_id} estas en la partida {lobby_id}", status_code=200)
 #en db_session, vez de hacer un db_player, tomo el id del jugador y modifico los campos que quiero
