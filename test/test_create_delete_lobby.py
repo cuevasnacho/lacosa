6c0cb770218829bd260f8a823e69d8d422af1403
bruno.volpini <bruno.volpini@mi.unc.edu.ar>
2023-10-07 17:15:14 -0300

    test que comprueban la funcionalidad de los modulos

---
+++ test/test_create_delete_lobby.py
@@ -0,0 +1,139 @@
+from api.models.lobby import Crear_Lobby, delete_lobby
+from fastapi.testclient import TestClient
+from unittest.mock import MagicMock, patch
+from main import app 
+from pony.orm import db_session
+from db.database import Lobby as db_lobby
+from db.database import Player
+import pytest
+
+client = TestClient(app)
+@patch('api.models.lobby')
+def test_create_delete_lobby(mock_create_lobby):
+    
+    response = client.post(
+    "/lobbys",
+    headers={"X-Token": "coneofsilence"},
+    json={
+  "player_id": 0, #el player no existe
+  "lobby_name": "dunga",
+  "lobby_max": 10,
+  "lobby_min": 6,
+  "lobby_password": "dunga"
+},
+    )
+    assert response.status_code == 404
+    assert response.json() == "Error creacion del lobby"
+
+
+    response = client.post(
+    "/lobbys",
+    headers={"X-Token": "coneofsilence"},
+    json={
+  "player_id": 1, #el player existe
+  "lobby_name": "dungaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+  "lobby_max": 10,
+  "lobby_min": 6,
+  "lobby_password": "dunga"
+},
+    )
+
+    assert response.status_code == 406
+    assert response.json() == "Nombre demasiado largo"
+
+
+    response = client.post(
+    "/lobbys",
+    headers={"X-Token": "coneofsilence"},
+    json={
+  "player_id": 1, #el player existe
+  "lobby_name": "dungaa",
+  "lobby_max": 10,
+  "lobby_min": 3,
+  "lobby_password": "dunga"
+},
+    )
+
+    assert response.status_code == 406
+    assert response.json() == "Minimo de players no permitido"
+
+    response = client.post(
+    "/lobbys",
+    headers={"X-Token": "coneofsilence"},
+    json={
+  "player_id": 1, #el player existe
+  "lobby_name": "dungaa",
+  "lobby_max": 15,
+  "lobby_min": 5,
+  "lobby_password": "dunga"
+},
+    )
+
+    assert response.status_code == 406
+    assert response.json() == "Maximo de players no permitido"
+
+    response = client.post(
+    "/lobbys",
+    headers={"X-Token": "coneofsilence"},
+    json={
+  "player_id": 1, #el player existe
+  "lobby_name": "dungaa",
+  "lobby_max": 6,
+  "lobby_min": 10,
+  "lobby_password": "dunga"
+},
+    )
+
+    assert response.status_code == 406
+    assert response.json() == "Minimo y maximo de players no permitido"
+
+    add_player = client.post(
+    "/players",
+    headers={"X-Token": "coneofsilence"},
+    json={"player_name" : "leo messi"},
+    )
+
+    response = client.post(
+    "/lobbys",
+    headers={"X-Token": "coneofsilence"},
+    json={
+  "player_id": 1, #el player existe en la db
+  "lobby_name": "dungaa",
+  "lobby_max": 11,
+  "lobby_min": 6,
+  "lobby_password": "dunga"
+},
+    )
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "lobby_id" : 2 #esta id siempre es par por que el primer test crea la entrada igual en la base de datos
+    }
+    
+    with db_session:
+        assert db_lobby.get(lobby_id = 2).lobby_name == "dungaa"
+        assert db_lobby.get(lobby_id = 0) == None
+        assert db_lobby.get(lobby_id = 2).lobby_player == {Player[1]}
+
+
+    response = client.delete(
+    "/lobbys/0",
+    headers={"X-Token": "coneofsilence"},
+    )
+
+    assert response.status_code == 404
+    assert response.json() == "El lobby no existe"
+    
+    response = client.delete(
+    "/lobbys/2",
+    headers={"X-Token": "coneofsilence"},
+    )
+
+    assert response.status_code == 200
+    assert response.json() == "lobby borrado!"
+
+
+    with db_session:
+        assert db_lobby.get(lobby_id = 2) == None
+
+    
---
+++ test/test_create_delete_player.py
@@ -0,0 +1,54 @@
+from api.models.user import Crear_Jugador, delete_player
+from fastapi.testclient import TestClient
+from unittest.mock import MagicMock, patch
+from main import app 
+from pony.orm import db_session, ObjectNotFound
+from db.database import Player as db_player
+import pytest
+
+client = TestClient(app)
+@patch('api.models.user')
+def test_create_delete_player(mock_create_player):
+    
+    response = client.post(
+    "/players",
+    headers={"X-Token": "coneofsilence"},
+    json={"player_name" : "leo messsssssssssssssssssssssssssssssssssssssssssssssssi"},
+    )
+    assert response.status_code == 406
+    assert response.json() == "Nombre demasiado largo"
+
+
+    
+    response = client.post(
+    "/players",
+    headers={"X-Token": "coneofsilence"},
+    json={"player_name" : "leo messi"},
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        "player_id" : 1, #esta id debe ser la proxima id que se autogenere, o se debe borrar la DB y poner 1
+        "player_name": "leo messi"
+    }
+
+    with db_session:
+        assert db_player.get(player_id = 1 ).player_name == "leo messi"
+        assert db_player.get(player_id = 0) == None
+
+
+    response = client.delete(
+    "/players/0",
+    headers={"X-Token": "coneofsilence"})
+    assert response.status_code == 404
+    assert response.json() == "El jugador no existe"
+
+    response = client.delete(
+    "/players/1", #mismo id que messi
+    headers={"X-Token": "coneofsilence"})
+    assert response.status_code == 200
+    assert response.json() == "Jugador borrado!"
+
+    with db_session:
+        assert db_player.get(player_id = 1) == None
+
+    
