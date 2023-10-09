41db8aa25440d454bdfed3b30dae71526afd83ff
Ignacio Cuevas <nachocuevas04@gmail.com>
2023-10-09 10:16:41 -0300

    Ajustando front

--- frontend/src/components/Lobby/Lobby.jsx
+++ frontend/src/components/Lobby/Lobby.jsx
@@ -1,66 +1,17 @@
-import CustomButton from '../Boton/CustomButton.jsx'
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import styles from './Lobby.module.css';
-import { httpRequest } from '../../services/HttpService.js';
 import JugadoresLobby from '../Lobby/JugadoresLobby.jsx';
 
 function Lobby({params}) {
     const { ws } = params;
-
-    const {idLobby} = useParams();
-
     const esHost = window.localStorage.getItem('Host');
+
+    const jugadores = useState([]);
 
     function Menu() {
         /* No hay un endpoint del back para volver al home*/
         alert("Volver al menu principal");
-    }
-
-    const refrescar = async () => {
-        try {
-          const response = await httpRequest({
-            method: 'GET',
-            service: `lobbys/${lobby_id}/refrescar`,
-          });
-          
-          window.localStorage.setItem('cantidadJugadores', response.lobby_pcount);
-
-          window.localStorage.setItem('jugadores', JSON.stringify(response.lobby_names));
-          
-          window.location.reload();
-    
-        } catch (error) {
-          console.log(error);
-        }
-    };
-
-    function IniciarPartida(idLobby) {
-        const cantidadJugadores = parseInt(window.localStorage.getItem('cantidadJugadores'));  
-        const cantidadMinima = parseInt(window.localStorage.getItem('minPlayers'));
-        
-        if (cantidadMinima <= cantidadJugadores) 
-        {
-            alert("Partida iniciada");
-
-            try 
-            {
-                const response = httpRequest({
-                    method: 'PUT',
-                    service: 'partida/iniciar/' + idLobby,
-                });
-                const idPartida = JSON.stringify(response.match_id);
-                
-                window.location = '/partida/' + idPartida;
-            } 
-            catch (error) 
-            {
-                console.log(error);                
-            }
-        }  
-        else 
-        {
-            alert("No se puede iniciar la partida, faltan jugadores");
-        }
     }
 
     ws.onmessage = function (event) {
@@ -68,7 +19,7 @@ function Lobby({params}) {
 
         switch (info.action) {
             case 'lobby_players':
-                window.localStorage.setItem('jugadores',info.data);
+                jugadores = JSON.stringify(info.data);
                 return;
             default:
                 return;
@@ -85,15 +36,10 @@ function Lobby({params}) {
             <div className={styles.container}>
                 <div className={styles.jugadores}>
                     <h1>Jugadores</h1>   
-                    <h3> {window.localStorage.getItem('cantidadJugadores')} </h3> 
-                    <JugadoresLobby jugadores={JSON.parse(window.localStorage.getItem('jugadores'))}/>
+                    <h3> {jugadores.length} </h3> 
+                    <JugadoresLobby jugadores={jugadores}/>
                 </div>
                 <button className={styles.botonPrueba} onClick={mandarMensaje} type='button'>Mandar</button>
-                <div className={styles.botones}>
-                    <CustomButton label="Volver al menu principal" onClick={Menu} />
-                    {esHost && <CustomButton label="Iniciar Partida" onClick={() => IniciarPartida(idLobby)} />}
-                    <CustomButton label="Refrescar" onClick={() => refrescar()} />
-                </div>
             </div>
         </>
     );
