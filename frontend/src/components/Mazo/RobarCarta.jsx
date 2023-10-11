213c957295bdc4f340b23c444b149f6b93719de5
irmrz <ignaciotramirez02@gmail.com>
2023-10-11 20:54:52 -0300

    metido en bloque try catch

--- frontend/src/components/Mazo/RobarCarta.jsx
+++ frontend/src/components/Mazo/RobarCarta.jsx
@@ -3,11 +3,19 @@ import { httpRequest } from "../..//services/HttpService";
 export async function robarCarta(mano, actualizarMano)
 {
     const user_id = window.sessionStorage.getItem('user_id');
-    const response = await httpRequest({
-        method: 'POST',
-        service: '/card/' + user_id
-    });
-    //const nuevaCarta = {cartaNombre: response.cartaNombre, id: response.id, tipo: response.tipo};
-    //const manoNueva = [...mano, nuevaCarta];
-    //actualizarMano(manoNueva);
+    try 
+    {
+        const response = await httpRequest({
+            method: 'POST',
+            service: '/card/' + user_id
+        });
+        const nuevaCarta = {cartaNombre: response.cartaNombre, id: response.id, tipo: response.tipo};
+        const manoNueva = [...mano, nuevaCarta];
+        actualizarMano(manoNueva);
+        
+    } 
+    catch (error) 
+    {
+        alert(error);
+    }
 }
