8da96785ebd8febcff04d485930193b421746c7e
Ignacio Cuevas <nachocuevas04@gmail.com>
2023-10-11 19:00:21 -0300

    Interfaz partida terminada

--- frontend/src/components/Partida/InfoPartida.jsx
+++ frontend/src/components/Partida/InfoPartida.jsx
@@ -1,6 +1,7 @@
 import Partida from './Partida.jsx';
 
 const InfoPartida = function () {
+  const user_id = parseInt(window.sessionStorage.getItem('user_id'));
   const ws = new WebSocket(`ws://localhost:8000/ws/${user_id}`);
 
   return (
--- frontend/src/components/Partida/Partida.jsx
+++ frontend/src/components/Partida/Partida.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './Partida.module.css';
 import ManoJugador from '../ManoJugador/ManoJugador.jsx';
 import Jugadores from '../Jugador/Jugadores.jsx';
@@ -10,29 +10,35 @@ function Partida () {
   // const user_id = JSON.parse(sessionStorage.getItem('user_id'));
   // const username = JSON.parse(sessionStorage.getItem('username'));
 
-  const [matchState, setMatchState] = useState([]);
-  const [mazoDescarteState, setMazoDescarteState] = useState(2);  // Dice que carta se va a mostrar en el mazo de descarte
+  const [matchState, setMatchState] = useState({});
+  const [mazoDescarteState, setMazoDescarteState] = useState(1);  // Dice que carta se va a mostrar en el mazo de descarte
   const [turno, setTurno] = useState(true);   // Indica si es mi turno o no
+  const [jugadores, setJugadores] = useState([]); // username: string, id: int, esTurno: bool, posicion: int, eliminado: bool	
+  const [cartas, setCartas] = useState([]); // cartas de la mano del jugador
+  const [listo, setListo] = useState(false); // cuando todos los jugadores estan listos comienza la partida
 
 /*
   ws.onmessage = function (event) {
     const info = JSON.parse(event.data);
     switch (info.action) {
-      case 'estado_jugadores':
-        setMatchState(info.data);
+      case 'cartas':
+        datos = info.data.cartas;
+        setCartas(datos);
         return;
+
+      case ''
 
       default:
         return;
     }
   }
 */
-  const cartas = [{cartaNombre: 'analisis', id: 2, tipo: 0},
+  const cartass = [{cartaNombre: 'analisis', id: 2, tipo: 0},
                   {cartaNombre: 'lacosa', id: 3, tipo: 0},
                   {cartaNombre: 'aterrador', id: 5, tipo: 0},
                   {cartaNombre: 'cuerdas_podridas', id: 1, tipo: 1}];
   
-  const jugadores = [ {username: 'juan', esTurno: false, position: 7},
+  const jugadoress = [ {username: 'juan', esTurno: false, position: 7},
                       {username: 'pedro', esTurno: false, position: 1},
                       {username: 'tute', esTurno: false, position: 2},
                       {username: 'nacho', esTurno: false, position: 3},
@@ -40,7 +46,7 @@ function Partida () {
                       {username: 'negro', esTurno: false, position: 5},
                       {username: 'quito', esTurno: false, position: 6}];
 
-  const sortedJugadores = jugadores.sort((a,b) => a.position - b.position);
+  const sortedJugadores = jugadoress.sort((a,b) => a.position - b.position);
 
   return (
     <div className={styles.container}>
@@ -48,7 +54,7 @@ function Partida () {
       <div className={styles.detalleMesa}/>
       <Mazo esTurno={turno}/>
       <MazoDescarte mazoDescarteState={mazoDescarteState}/>
-      <ManoJugador cartas={cartas} esTurno={turno}/>
+      <ManoJugador cartas={cartass} esTurno={turno}/>
       <Jugadores jugadores={sortedJugadores}/>
     </div>
   );
