a88b64fbc74b0c674e3bd39fb2ce8607b8bb5a18
timoh <tomashubman47@gmail.com>
2023-10-11 22:37:25 -0300

    test de robar carta casi finalizado

--- api/player/steal_card.py
+++ api/player/steal_card.py
@@ -4,7 +4,6 @@ from fastapi.responses import JSONResponse
 from db.database import Match, Player, Card
 from pony.orm import db_session, commit ,select, ObjectNotFound
 from definitions import match_status , card_position
-from db.database import Player
 
 router = APIRouter()
 
@@ -13,33 +12,6 @@ class carta_robada(BaseModel):
     id : int
     tipo : bool
 
-
-@db_session
-def discard_to_deck(match_id):
-    discard = card_posible_query = Card.select(lambda c : c.card_match.match_id == match_id and
-                           c.card_location == card_position.DISCARD.value)
-    match = get_match(match_id)
-
-    for card in discard:
-        card.card_location = card_position.DECK.value
-        match.match_cardsCount += 1
-        commit()
-
-
-@db_session
-def get_card_from_deck(match_id):
-        card_steal = Card.select(lambda c : c.card_match.match_id == match_id and
-                           c.card_location == card_position.DECK.value).random(1)[0]
-
-        if (card_steal is None):
-            discard_to_deck(match_id)
-            card_steal = Card.select(lambda c : c.card_match.match_id == match_id and
-                           c.card_location == card_position.DECK.value).random(1)[0]
-
-        if card_steal is None:
-            raise ObjectNotFound("No hay cartas asociadas a la partida")
-    
-        return card_steal
 
 @db_session
 def get_match(id_match):
@@ -59,13 +31,41 @@ def get_player(id_player):
 
     return player
 
+@db_session
+def discard_to_deck(match_id):
+    discard = list(Card.select(lambda c : c.card_match.match_id == match_id and
+                           c.card_location == card_position.DISCARD.value))
+    match = get_match(match_id)
+    
+    for card in discard:
+        card.card_location = card_position.DECK.value
+        match.match_cardsCount += 1
+        commit()
+
+
+@db_session
+def get_card_from_deck(match_id):
+        deck_cards = Card.select(lambda c : c.card_match.match_id == match_id and
+                           c.card_location == card_position.DECK.value)
+
+        if not deck_cards:
+            discard_to_deck(match_id)
+            deck_cards = Card.select(lambda c : c.card_match.match_id == match_id and
+                           c.card_location == card_position.DECK.value)
+
+        if deck_cards :
+            card_steal = deck_cards.random(1)[0]
+            return card_steal
+
+
+        return deck_cards
+
 @router.post("/card/{player_id}")
 async def steal_card(player_id : int)-> carta_robada:
     with db_session:
         try:
     
             player = Player.get(player_id = player_id)
-            print(f"player {player_id} is  = {player}") 
 
             if player is None:
                 content = "El jugador no existe"
@@ -76,6 +76,7 @@ async def steal_card(player_id : int)-> carta_robada:
                 return JSONResponse(content = content, status_code = 406)
             
             match = Match.get(match_id=player.player_current_match_id.match_id)
+            
             if match is None:
                 content = "La partida asociada al jugador no existe"
                 return JSONResponse(content = content, status_code = 404)
@@ -84,13 +85,14 @@ async def steal_card(player_id : int)-> carta_robada:
                 content = "La partida no esta inicializada o ya finalizo"
                 return JSONResponse(content = content, status_code = 406)
 
-            if (player.player_id == match.match_currentP):
+            
+            if (player.player_id != match.match_currentP):
                 content = "No es el turno del jugador"
                 return JSONResponse(content = content, status_code = 406)
             
             card = get_card_from_deck(match.match_id)
 
-            if card is None:
+            if not card:
                 content = "No hay cartas asociadas a la partida"
                 return JSONResponse(content = content, status_code = 404)
 
