3d081246fe0261e8471d72942a25e32aeb156922
Marcos Strasorier <marcos.strasorier@mi.unc.edu.ar>
2023-10-11 23:12:49 -0300

    Nada, Solo modulo

---
+++ api/lobby/leave_lobby.py
@@ -0,0 +1,4 @@
+#en este modulo debo implementar el endpoint y la logica correspondiente para que un jugador tenga la posibilidad
+#de abandonar un lobby, para esto debo modificar el estado del jugador y del lobby, en el caso del jugador (fastapi)
+
+from fastapi import APIRouter
