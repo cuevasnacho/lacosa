d58ca4c7b4e8651eeae1737eda9180a055f130a4
Ignacio Cuevas <nachocuevas04@gmail.com>
2023-10-16 17:44:08 -0300

    Test ManoJugador terminado

--- frontend/src/components/ManoJugador/ManoJugador.jsx
+++ frontend/src/components/ManoJugador/ManoJugador.jsx
@@ -10,7 +10,7 @@ function ManoJugador({ cartas, esTurno , actualizar}) {
         <div
           key={index}
           style={{ position: 'relative' }}
-          data-testid={carta.nombre}
+          data-testid="carta"
         >
           <Carta carta={carta} esTurno={esTurno} actualizar={actualizar} mano={cartas} />
         </div>
