9be07bd16ac87bfaf4a485764a2dd10e599c205a
Tomas Marmay <tomasmarmay21@gmail.com>
2023-10-20 20:44:25 -0300

    finalizar partida front

---
+++ frontend/src/components/FinalizarPartida/Finalizar.jsx
@@ -0,0 +1,62 @@
+import { Link } from "react-router-dom"
+import  "./Finalizar.css"
+import { useEffect,useState } from "react"
+import { httpRequest } from '../../services/HttpService.js'
+
+export default function Finalizar({idPartida}) {
+    const [jugadores,setJugadores]=useState([])
+    const [ganador,setGanador]=useState("")
+    useEffect(() => {
+        const getJugadores = async () => {
+            let headers = {
+                Accept: '*/*', 
+            }
+            try 
+            {
+              const data = await httpRequest({ method: 'GET',headers:headers, service: `partida/resultado/${idPartida}`});
+              console.log(data)
+              setJugadores(data.jugadores);
+              setGanador(data.ganadores)
+            } 
+            catch (error) {
+              console.log(error);
+            }
+          }      
+        getJugadores()
+     }, [])
+    
+  return (
+    <article className="modal">
+        <div className='modal-container'>
+        <h1 className="titulo-tabla">Victoria para los {ganador}</h1>
+            <div className="container-tabla">      
+                <div className='tabla-unirsePartida'>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Nombre del Jugador</th>
+                                <th>Equipo</th>
+                            </tr>
+                        </thead>
+                        <tbody> 
+                            {jugadores.length > 0 ?(
+                                jugadores.map((jugadores,index=uuid()) => (
+                                <tr key={index}>
+                                    <td>{jugadores.nombre_jugador}</td>
+                                    <td>{jugadores.equipo}</td>  
+                                </tr>
+                                )))
+                                : (
+                                <tr>
+                                    <td colSpan={2}>No hay jugadores ganadores</td>
+                                </tr>
+                                )}
+                        </tbody>
+                    </table>
+                </div> 
+            </div>
+            <Link className="boton-terror" to="/home">Volver Inicio</Link>
+        </div>
+    </article>
+  )
+}
