bec5dcaa5d860ffa6f02071c824fe339165f2487
Tomas's Home <tomasmarmay21@gmail.com>
2023-11-02 20:44:30 -0300

    logica implementada falta probar

--- api/player/exchange.py
+++ api/player/exchange.py
@@ -6,77 +6,95 @@ from api.player.steal_card import get_match
 from db.database import Player, Match, Card
 from pony.orm import db_session, commit
 from api.card.alejate import adjacent_players
+from definitions import card_position,cards_subtypes,results, player_roles
 
 router = APIRouter()
-#reason == 0 -> causaso normales
-#reaso == 1 -> por carta seduccion
 
+#POSIBLE ERROR ES QUE NO ESTEN CON LOS MISMOS NOMBRES
 @db_session
-def valid_card(card_id):
-    not_valid_cards = ["la_cosa", "infectado"] #OJO QUE ESTEN LOS MISMOS NOMBRES, #FALTA LOGICA DE INFECTADO
+def valid_card(card_id,role):
+    if role == player_roles.THE_THING.value:
+        not_valid_cards = ["la_cosa"]
+    elif role == player_roles.HUMAN.value:
+        not_valid_cards = ["infectado"] 
+    elif role == player_roles.INFECTED.value:
+        not_valid_cards = []
+
     card = Card[card_id]
     if card.card_cardT.cardT_name in not_valid_cards: 
         return False 
     return True
 
 @db_session
-def valid_oponent(oponent_id,left,right,reason):
-    valid = True
+def valid_oponent(player_id,oponent_id,role,oponent_at_left,oponent_at_right,card_id):
     oponent = Player[oponent_id]    
-    if reason:
-        if oponent.player_quarentine_count > 0 or ((not oponent.player_exchangeL) and left) or ((not oponent.player_exchangeR) and right):
-            valid = False
-    else: 
-        if oponent.player_quarentine_count > 0 :
-            valid = False
+    card = Card[card_id]
+    oponent = Player[oponent_id]
+    oponent_role = oponent.player_role
+    valid = True
+
+    if role != player_roles.THE_THING.value:
+        player_hand = Card.select(lambda card : card.card_player.player_id == player_id and 
+                                    card.card_cardT.cardT_name == "infectado") #ver que funcione
+        infect_card = 0
+        for card in player_hand:
+            infect_card += 1
+
+        #no tengo mas de una carta infectado o no se la doy a la cosa
+        if card.card_cardT.cardT_name == "infectado":
+            if role == player_roles.INFECTED.value:
+                if infect_card <= 1 or oponent_role != player_roles.THE_THING.value:
+                    valid = valid and False 
+
+        #no se da el intercambio si tengo todas cartas de infectado -> superinfeccion
+        if role == player_roles.HUMAN.value:
+            if infect_card == 6:
+                valid = valid and False 
+    
+    #carta no es seducion -> derecha o izq
+    if card.card_cardT.cardT_name != "seduccion:":
+        if not (oponent_at_left or oponent_at_right): #el jugador no es adyecente
+            valid = valid and False 
+
+    #caso puerta atrancada
+    if ((not oponent.player_exchangeL) and oponent_at_left) or ((not oponent.player_exchangeR) and oponent_at_right):
+        valid = valid and False    
+
     return valid
 
+#encargado de ver si se cumplen todas las condiciones para poder intercambiar
 @router.get("/intercambio/peticion/{player_id}/{oponent_id}/{player_card_id}/{reason}")
 async def exchange_petition(player_id : int, oponent_id : int, player_card_id : int, reason : int):
     try: 
         player = Player[player_id]
-        player_card = Card[player_card_id]
-        oponent = Player[oponent_id]
     except:
         content = "El objeto no existe"
         return JSONResponse(content = content, status_code = 404)
-
-    #valida la carta 
-    if not valid_card:
-        return JSONResponse(content = "Carta invalida", status_code = 400)
     
-    #valida al jugador oponente 
-    adjacent = adjacent_players(player_id,oponent_id) 
-    if not valid_oponent(oponent_id,adjacent[0],adjacent[1]):
-        return JSONResponse(content = "Jugador invalida", status_code = 400)
-    
-    #tiene que intercambiar con alguien del costado
-    if (not (adjacent[0] or adjacent[1])) and reason:
-        return JSONResponse(content = "El jugador no es adyecente", status_code = 400)
-        
-    #enviar mensaje por socket de peticion de intercambio 
-    return JSONResponse(content = "Succes", status_code = 200)
+    oponent_position = adjacent_players(player_id,oponent_id) 
+    is_card_valid = valid_card(player_card_id,player.player_role)
+    is_oponent_valid = valid_oponent(player_id,oponent_id,player.player_role,oponent_position[0],oponent_position[1],player_card_id)
+    #mandar mensaje por soccket
+    exchange = is_card_valid and is_oponent_valid
+    return JSONResponse(content = exchange, status_code = 200)
 
 @db_session
-def have_defense_card(oponent_id,reason):
-    not_valid_cards = ["aterrador","no_gracias","fallaste"] #OJO QUE ESTEN LOS MISMOS NOMBRES, FALTA IMPLEMETAR FALLASTE 
+def have_defense_card(oponent_id):
+    defense_cards = ["aterrador","no_gracias","fallaste"]  
     cards = Card.select(lambda card : card.card_player == oponent_id)
     for card in cards : 
-        if card.card_cardT.cardT_name in not_valid_cards:
+        if card.card_cardT.cardT_name in defense_cards:
             return True 
     return False
 
-@router.get("/intercambio/aceptacion/{player_id}/{card_id}/{reason}")
-async def exchange_accept(player_id : int, card_id : int, reason : int):
+#encargado solamente de chequear si se puede defender
+@router.get("/intercambio/aceptacion/{player_id}/{card_id}")
+async def exchange_accept(player_id : int, card_id : int):
     #player_id es el oponent del endpoind anterior 
-    contet = False
-    if have_defense_card(player_id,reason):
-        contet = True    
+    defense = have_defense_card(player_id)
+    return JSONResponse(content = defense, status_code = 200)
 
-        #si el jugador se defiende no se genera el intercambio
-    #si la carta no se valida no se genera el intercambio
-    return JSONResponse(content = contet, status_code = 200)
-
+#swap de cartas 
 @router.put("/intercambio/cartas/{match_id}/{player_id}/{card1_id}/{oponent_id}/{card2_id}")
 async def exchange_accept(match_id: int, player_id : int, card1_id : int, oponent_id : int, card2_id : int):
     try: 
