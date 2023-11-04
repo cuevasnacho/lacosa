568af5c59f483ee270ba3c7409a4226d4cb5a333
Tomas Marmay <tomasmarmay21@gmail.com>
2023-11-04 19:51:12 -0300

    base de test de intercambio

---
+++ test_exchange.py
@@ -0,0 +1,80 @@
+import pytest
+from unittest.mock import MagicMock
+from unittest.mock import patch
+from api.player.exchange import swap_cards,exchange_defense,exchange_valid
+from fastapi.testclient import TestClient
+from main import app 
+import json 
+import os 
+import subprocess
+import time 
+from db.database import Card
+from pony.orm import db_session
+from api.card.load_templates import load_templates
+
+client = TestClient(app)
+
+def set_env(file):
+    database = "db/lacosa.sqlite"
+    create_database_command = "python3 db/database.py"
+    get_into_database = "sqlite3 db/lacosa.sqlite"
+    file_entrys = file
+    if os.path.exists(database):
+        os.remove(database)
+        time.sleep(0.1)
+
+    subprocess.run(create_database_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    time.sleep(0.1)
+
+    with open(file_entrys, 'r') as file:
+    # Itera sobre cada línea del archivo
+        for line in file:
+            # Procesa la línea, por ejemplo, imprimiéndola en la consola
+            command = get_into_database + " " + line.strip() 
+            print(command)
+            subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            time.sleep(0.1)
+
+def set_env_2(file):
+    database = "db/lacosa.sqlite"
+    create_database_command = "python3 db/database.py"
+    get_into_database = "sqlite3 db/lacosa.sqlite"
+    file_entrys = file
+
+    subprocess.run(create_database_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    time.sleep(0.1)
+
+    with open(file_entrys, 'r') as file:
+    # Itera sobre cada línea del archivo
+        for line in file:
+            # Procesa la línea, por ejemplo, imprimiéndola en la consola
+            command = get_into_database + " " + line.strip() 
+            print(command)
+            subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            time.sleep(0.1)
+
+'''
+#intercambio de cartas
+def test_swap_cards():
+    set_env("db/test_swap_cards.txt")
+    #intercambio
+    time.sleep(1)
+    response = client.put("/intercambio/cartas/1/1/2/2") #jugador 1 tiene carta 1 y jugador 2 tiene carta 2
+    time.sleep(1)
+    with db_session:
+        #jugador 1 tiene carta 2
+        player1_card = Card.select(lambda card : card.card_id == 1).first()
+        assert player1_card.card_player.player_id == 2
+        #jugador 2 tiene carta 1
+        player2_card = Card.select(lambda card : card.card_id == 2).first()
+        assert player2_card.card_player.player_id == 1
+
+        #assert json.loads(response.content) == "Cambio realizado"
+'''
+
+def test_exchange_defense():
+    #load_templates()
+    #set_env_2("db/test_exchange_defense.txt")
+    #time.sleep(1)
+    response = client.get("/intercambio/defensa/1")
+    #assert response.content == True
