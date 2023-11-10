c6be851af2bcf9ac00df0ceb09d5bdad130b46e8
irmrz <ignaciotramirez02@gmail.com>
2023-11-09 21:25:00 -0300

    funciona a medias

--- frontend/src/components/Carta/Defensa.jsx
+++ frontend/src/components/Carta/Defensa.jsx
@@ -1,31 +1,35 @@
 import {  useEffect, useState } from 'react';
-import { Modal, ModalHeader, ModalBody, Button, ModalFooter } from 'reactstrap';
+import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import { httpRequest } from '../../services/HttpService';
 import { getHand } from '../Partida/functions';
+import CustomButton from '../Boton/CustomButton';
 
-function Defensa({dataSocket, manoJugador, stage, setManoJugador, socket})
+function Defensa({dataSocket, manoJugador, setManoJugador, socket})
 {
+    console.log(dataSocket);
     const defense_card_list = dataSocket.card_to_defend;
+    console.log('Cartas para defenderse: '+ defense_card_list);
     const attacker = dataSocket.attacker_id;
-    const attack_card_name = dataSocket.atack_card_name;
+    console.log('atacante: '+ attacker);
+    const attack_card_name = dataSocket.attack_card_name;
 
-    const [modal, setModal] = useState(true);
+    const [modal, setModal] = useState(false);
     const toggle = () => setModal(!modal);
 
     const backdrop = false;
     
     const defensor_id = window.sessionStorage.getItem('user_id');
-    let defenseCardId = null;
 
     useEffect(() => {
-        if (stage === 4) {
-            toggle();
-        }
+        toggle();
     }, []);
 
-    async function handleDefensa(defenseCardName, attacker_id)
+    async function handle_defensa(defenseCardName, attacker_id)
     {
-        defenseCardId = getDefenseCardId(defenseCardName, manoJugador);
+        const defenseCardId = get_defense_card_id(defenseCardName, manoJugador);
+        console.log('defenseCardId:' + defenseCardId);
+        console.log('attacker:' + attacker_id);
+        
         if (defenseCardId === null) {
             alert("Hubo un error al obtener el id de la carta de defensa");
         }
@@ -36,19 +40,19 @@ function Defensa({dataSocket, manoJugador, stage, setManoJugador, socket})
         });
 
         getHand(setManoJugador);
-
         toggle();
     }
 
-    function getDefenseCardId(defenseCardName, manoJugador) {
+    function get_defense_card_id(defenseCardName, manoJugador) {
         for (const card of manoJugador) {
+            console.log('cardName: ' + card.cardName)
             if (card.cardName === defenseCardName) {
                 return card.id;
             }
         }
     }
 
-    function noDefense() {
+    function no_defense() {
         const mensaje_no_defense = JSON.stringify({
             action: 'no_defense', 
             data: {defensor_id: defensor_id, 
@@ -58,9 +62,10 @@ function Defensa({dataSocket, manoJugador, stage, setManoJugador, socket})
         toggle();
     }
 
+    //<CustomButton label={'Defenderse'} onClick={toggle}></CustomButton>
     return(
         <>
-        <Modal isOpen={modal} toggle={toggle} backdrop={backdrop} onExit={noDefense} >
+        <Modal isOpen={modal} toggle={toggle} backdrop={backdrop} onExit={no_defense} centered >
             <ModalHeader toggle={toggle}> Defensa</ModalHeader>
             <ModalBody>
                 Has sido atacado con {attack_card_name}, te podés defender con:
@@ -68,16 +73,12 @@ function Defensa({dataSocket, manoJugador, stage, setManoJugador, socket})
                     {defense_card_list.map((card, index) => (
                         <li key={index}>
                             {card}
-                            <Button onClick={() => handleDefensa(card, attacker)}>
-                                Usar
-                            </Button>
+                            <CustomButton onClick={() => handle_defensa(card, attacker)} label={'Usar'}/>
                         </li>))}
                 </ul>
             </ModalBody>
             <ModalFooter>
-                <Button color='secondary' onClick={noDefense}>
-                    No defenderse
-                </Button>
+                <CustomButton label={'No Defenderse'} onClick={no_defense}/>
             </ModalFooter>
         </Modal></>
     );
