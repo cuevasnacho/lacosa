36d4e4b100d7f99984366ebf071095fdf6cd2428
bruno.volpini <bruno.volpini@mi.unc.edu.ar>
2023-11-13 21:57:42 -0300

    anda test

--- test/test_create_delete_player.py
+++ test/test_create_delete_player.py
@@ -5,10 +5,35 @@ from main import app
 from pony.orm import db_session, ObjectNotFound
 from db.database import Player as db_player
 import pytest
+import os 
+import subprocess
+import time 
+
+def set_env():
+    database = "db/lacosa.sqlite"
+    create_database_command = "python3 db/database.py"
+    get_into_database = "sqlite3 db/lacosa.sqlite"
+    file_entrys = "db/test_create_delete_player.txt"
+    if os.path.exists(database):
+        os.remove(database)
+        time.sleep(0.1)
+
+    subprocess.run(create_database_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    time.sleep(0.1)
+
+    with open(file_entrys, 'r') as file:
+    # Itera sobre cada línea del archivo
+        for line in file:
+            # Procesa la línea, por ejemplo, imprimiéndola en la consola
+            command = get_into_database + " " + line.strip() 
+            print(command)
+            subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            time.sleep(0.1)
 
 client = TestClient(app)
-@patch('api.player.player')
-def test_create_delete_player(mock_create_player):
+def test_create_delete_player():
+    set_env()
+    time.sleep(0.1)
     
     response = client.post(
     "/players",
@@ -27,12 +52,12 @@ def test_create_delete_player(mock_create_player):
     )
     assert response.status_code == 200
     assert response.json() == {
-        "player_id" : 2, #esta id debe ser la proxima id que se autogenere, o se debe borrar la DB y poner 1
+        "player_id" : 3, #esta id debe ser la proxima id que se autogenere, o se debe borrar la DB y poner 1
         "player_name": "leo messi"
     }
 
     with db_session:
-        assert db_player.get(player_id = 2 ).player_name == "leo messi"
+        assert db_player.get(player_id = 3 ).player_name == "leo messi"
         assert db_player.get(player_id = 0) == None
 
 
@@ -49,9 +74,7 @@ def test_create_delete_player(mock_create_player):
     assert response.json() == "Jugador borrado!"
 
     with db_session:
-        assert db_player.get(player_id = 2) == None
-    set_env()
-
+        assert db_player.get(player_id = 1) == None
 # para correr:
 #1 borrar lacosa.sqlite
 #2 compilar python3 database.py
